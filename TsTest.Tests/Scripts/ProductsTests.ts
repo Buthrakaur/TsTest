0798b7af5a369d5a2e04d092fa77b046571d3000
Filip Kinský <filip@filovo.net>
2012-11-04 23:03:34 +0100

    TsTest.Tests - rozchozen Jasmine + QUnit

---
+++ TsTest.Tests/Scripts/ProductsTests.ts
@@ -0,0 +1,8 @@
+///<reference path="libs/jasmine.d.ts" />
+///<reference path="libs/qunit-1.10.d.ts" />
+
+describe("A suite", () => {
+    it("contains spec with an expectation", () => {
+        expect(true).toBe(true);
+    });
+});
