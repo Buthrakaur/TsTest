0e7ea59ff32a0c4d1a7a854b22cadfff0a2678ba
Filip Kinský <filip@filovo.net>
2012-11-05 20:36:11 +0100

    oprava testu

--- TsTest.Tests/Scripts/ProductsTests.js
+++ TsTest.Tests/Scripts/ProductsTests.js
@@ -1,6 +1,7 @@
 describe("Product tests", function () {
-    var p = new Product(1, 'Product 1');
+    var p = new Products.Product(1, 'Product 1');
     it("Product should be initialized", function () {
         expect(p.productNumber).toBe(1);
+        expect(p.name).toBe("Product 1");
     });
 });
--- TsTest.Tests/Scripts/ProductsTests.ts
+++ TsTest.Tests/Scripts/ProductsTests.ts
@@ -1,11 +1,12 @@
-///<reference path="../../TsTest/Scripts/Products.js" />
+///<reference path="../../TsTest/Scripts/Products.ts" />
 ///<reference path="libs/jasmine.d.ts" />
 ///<reference path="../../TsTest/Scripts/libs/knockout-2.2.d.ts" />
 
 describe("Product tests", () => {
-    var p = new Product(1, 'Product 1');
+    var p = new Products.Product(1, 'Product 1');
 
     it("Product should be initialized", () => {
         expect(p.productNumber).toBe(1);
+        expect(p.name).toBe("Product 1");
     });
 });
